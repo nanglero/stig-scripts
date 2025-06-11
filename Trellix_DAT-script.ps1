6125eafdfb6146488fcd651ff6f132458e71c176
nanglero <58918183+nanglero@users.noreply.github.com>
2025-06-11 16:55:59 -0400

    Create Trellix_DAT-script.ps1
    Patch install for Trellix

---
+++ Trellix_DAT-script.ps1
@@ -0,0 +1,25 @@
+#Trellix DAT latest patching script
+$installerPath = "C:\Users"
+
+#Step 1: verify that the installer exists
+if (Test-Path $installerPath) {
+    Write-Path "Installer found at $installPath. Proceeding with installation..."
+    exit
+    } else {
+    Write-Host "Installer not found at the specific path. Please check the file location."
+    exit
+    }
+
+    #step 2: Install Trellix DAT silently
+    Write-Host "Installing Trellix DAT silently..."
+    Start-Process -FilePath $installerPath -ArgumentList "/sAll /rs /msi /norestart" -wait
+
+    #step 3: Verify if installation was successful
+    $TrellixDatInstalled = Get-WmiObject -Class win32_Product | Where-Object { $_.Name -like "Trellix DAT"}
+
+    if ($McafeePlatformInstalled) {
+    Write-Host "Trellix DAT installed successfully." 
+    }else {
+    Write-Host "Trellix DAT installation failed." 
+    }
+    write-Host "Script execution completed"
